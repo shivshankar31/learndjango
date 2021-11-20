e6ac847ec692acc2ad69196dc8a32052df2896dc
shivshankar31 <s_shankar12@hotmail.com>
2021-11-21 05:22:10 +0530

    Add More Views

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 
 urlpatterns = [
-    path('january', views.index)
+    path('january', views.january),
+    path('february', views.february),
+    path('march', views.march)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('This works!')
-    
+def january(request):
+    return HttpResponse('This in january, Always dream your dream!')
+
+
+def february(request):
+    return HttpResponse('This is february, Work smart and earn better!')
+
+def march(request):
+    return HttpResponse('This is march, Run with the phase, do not stop when you fail!')
