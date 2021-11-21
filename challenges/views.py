353de57863abc63061e47c336dcfe0fdac10b3e0
shivshankar31 <s_shankar12@hotmail.com>
2021-11-21 06:39:50 +0530

    25 dynamic view using dict and add try block

--- challenges/views.py
+++ challenges/views.py
@@ -2,32 +2,37 @@ from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
-# now with below function integers also handled 
+# 25 adding more dynamic view logic 
+
+# create a dict for all 12 months
+
+monthly_challenges_text = {
+    'january' : 'This in january, Always dream your dream!',
+    'february' : 'This is february, Work smart and earn better!',
+    'march' : 'This is march, Run with the phase, do not stop when you fail!',
+    'april' : 'This in april, Always dream your dream!',
+    'may' : 'This is may, Work smart and earn better!',
+    'june' : 'This is june, Run with the phase, do not stop when you fail!',
+    'july' : 'This in july, Always dream your dream!',
+    'august' : 'This is august, Work smart and earn better!',
+    'september' : 'This is september, Run with the phase, do not stop when you fail!',
+    'october' : 'This in october, Always dream your dream!',
+    'november' : 'This is november, Work smart and earn better!',
+    'december' : 'This is december, Run with the phase, do not stop when you fail!',
+    
+}
 
 def monthly_challenge_as_int(request, month):
     return HttpResponse(month)
 
+# call created dict with below function and add try block to handle error
+    
 
 def monthly_challenges(request, month):
-    if month == 'january':
-        return HttpResponse('This in january, Always dream your dream!') 
-    elif month == 'february':
-        return HttpResponse('This is february, Work smart and earn better!')
-    elif month == 'march':
-        return HttpResponse('This is march, Run with the phase, do not stop when you fail!')
-    else:
-        return HttpResponseNotFound('This month is not included')
+    try:
+        challenge_text = monthly_challenges_text[month]
+    except:
+        return HttpResponseNotFound('Type month propery!')
+    # return HttpResponseNotFound('This month is not included')
     
-# it can be return using a variable as well, 
-# def monthly_challenges(request, month):
-#     challenge_text = None
-#     if month == 'january':
-#         challenge_text = 'This in january, Always dream your dream!' 
-#     elif month == 'february':
-#         challenge_text = 'This is february, Work smart and earn better!' #this text can be given inside a tupple as well see below 
-#     elif month == 'march':
-#         challenge_text = ('This is march, Run with the phase, do not stop when you fail!')
-#     else:
-#         return HttpResponseNotFound('This month is not included')
-    
-#     return HttpResponse(challenge_text)
+    return HttpResponse(challenge_text)
