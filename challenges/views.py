401f484b4ad31709818fd3a215c2eef26ef19ceb
shivshankar31 <s_shankar12@hotmail.com>
2021-11-21 05:44:57 +0530

    23 Dynamic Path Segments & Captured Values

--- challenges/views.py
+++ challenges/views.py
@@ -1,14 +1,31 @@
+from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse('This in january, Always dream your dream!')
+# handling dynamic views using if and return 
 
-
-def february(request):
-    return HttpResponse('This is february, Work smart and earn better!')
-
-def march(request):
-    return HttpResponse('This is march, Run with the phase, do not stop when you fail!')
+def monthly_challenges(request, month):
+    if month == 'january':
+        return HttpResponse('This in january, Always dream your dream!') 
+    elif month == 'february':
+        return HttpResponse('This is february, Work smart and earn better!')
+    elif month == 'march':
+        return HttpResponse('This is march, Run with the phase, do not stop when you fail!')
+    else:
+        return HttpResponseNotFound('This month is not included')
+    
+# it can be return using a variable as well, 
+# def monthly_challenges(request, month):
+#     challenge_text = None
+#     if month == 'january':
+#         challenge_text = 'This in january, Always dream your dream!' 
+#     elif month == 'february':
+#         challenge_text = 'This is february, Work smart and earn better!' #this text can be given inside a tupple as well see below 
+#     elif month == 'march':
+#         challenge_text = ('This is march, Run with the phase, do not stop when you fail!')
+#     else:
+#         return HttpResponseNotFound('This month is not included')
+    
+#     return HttpResponse(challenge_text)
