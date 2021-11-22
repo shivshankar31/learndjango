5787bb451898fb5077aad5d2e0c53b3e146e673f
shivshankar31 <shivshankar31@yahoo.com>
2021-11-22 09:18:13 +0530

    37 using filter inside html file

--- challenges/views.py
+++ challenges/views.py
@@ -60,10 +60,11 @@ def monthly_challenge_as_int(request, month):
 def monthly_challenges(request, month):
     try:
         challenge_text = monthly_challenges_text[month]
-        cap = month.capitalize()
+        # instead of capitialize() here we can use django filterto formate inside html file  
+        # cap = month.capitalize()
         # 'render' is a second option insted of 'render_to_string', render is imported from django.shortcutes
         # to convert dynamic html page we use third arg with the dict as below, this 'text' name will be added to the html page with django syntax
-        return render(request, 'challenges/challenge.html', {'text': challenge_text, 'month': cap}) 
+        return render(request, 'challenges/challenge.html', {'text': challenge_text, 'month': month}) 
 
         #why we create challenges folder inside templetes, it is to avoide file duplications, its a best pratice to follow
         # html_response = render_to_string('challenges/challenge.html') # this is to call the html file after this we need to register the app with django setting.py file 
